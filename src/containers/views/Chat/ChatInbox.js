9f2482aa255c7e7420137cde0980009756369664
Lam Ngo <lam.ngo.qu@gmail.com>
2018-05-08 00:09:23 +0300

    Modified FlatList and Add ScrollView to render 2 consecutive lists

--- src/containers/views/Chat/ChatInbox.js
+++ src/containers/views/Chat/ChatInbox.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  ScrollView,
+} from 'react-native';
 import { SearchBar } from 'react-native-elements';
 
 import rest from '../../../utils/rest';
@@ -40,15 +46,34 @@ export class ChatInbox extends React.Component {
 
   flatList = (title, data) => {
     return (
-      <View>
-        <RoundTab tint="#ffffff" title={title} fontSize="12" />
-        <FlatList
-          data={data}
-          keyExtractor={this.keyExtractor}
-          renderItem={this.renderItem}
-          style={{ flex: 1, backgroundColor: 'white', minHeight: 300 }}
-        />
-      </View>
+      <FlatList
+        data={data}
+        keyExtractor={this.keyExtractor}
+        renderItem={this.renderItem}
+        style={{ flex: 1, backgroundColor: 'white' }}
+        ListHeaderComponent={this.renderHeader(title)}
+        scrollEnabled={false}
+      />
+    );
+  };
+
+  renderHeader = title => {
+    return (
+      <Text
+        style={{
+          marginTop: 20,
+          marginBottom: 20,
+          justifyContent: 'center',
+          height: 27,
+          fontFamily: 'NunitoSans-Regular',
+          fontSize: 20,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          backgroundColor: 'transparent',
+        }}
+      >
+        {title}
+      </Text>
     );
   };
 
@@ -77,12 +102,13 @@ export class ChatInbox extends React.Component {
     };
     searchResult.chatrooms = chatrooms.filter(
       chatroom =>
-        chatroom.creator.username
-          .toLowerCase()
-          .indexOf(this.state.searchKeyword) !== -1 ||
-        chatroom.receiver.username
-          .toLowerCase()
-          .indexOf(this.state.searchKeyword) !== -1,
+        chatroom.creator.id === this.props.currentUserId
+          ? chatroom.receiver.username
+              .toLowerCase()
+              .indexOf(this.state.searchKeyword) !== -1
+          : chatroom.creator.username
+              .toLowerCase()
+              .indexOf(this.state.searchKeyword) !== -1,
     );
     return searchResult;
   };
@@ -101,18 +127,18 @@ export class ChatInbox extends React.Component {
         this.sortChatrooms(a, b),
       );
       displayInboxCard = (
-        <View>
+        <ScrollView>
           {searchResult.chatrooms.length > 0 ? (
             this.flatList('CHATS', sortedChatrooms)
           ) : (
             <View />
           )}
-          {searchResult.messages.length > 0 ? (
+          {searchResult.chatrooms.length > 0 ? (
             this.flatList('MESSAGES', sortedChatrooms)
           ) : (
             <View />
           )}
-        </View>
+        </ScrollView>
       );
     } else {
       let sortedChatrooms = chatrooms.sort((a, b) => this.sortChatrooms(a, b));
