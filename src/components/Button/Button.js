d116c5aa2e354cca831c9fd03256ce48d3e86fe1
chengjo0 <john.cheng@futurice.com>
2018-05-21 18:29:06 +0300

    Button style updated for different size and color

---
+++ src/components/Button/Button.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
+
+class Button extends Component {
+  render() {
+    const { primary, text, width } = this.props;
+
+    let style = primary ? styles.button : styles.buttonSecondary;
+    let buttonTextStyle = primary
+      ? styles.buttonText
+      : styles.buttonTextSecondary;
+    let buttonWidth;
+
+    switch (width) {
+      case 'sm':
+        buttonWidth = 100;
+        break;
+      case 'md':
+        buttonWidth = 150;
+        break;
+      case 'xl':
+        buttonWidth = 200;
+        break;
+    }
+
+    return (
+      <TouchableOpacity style={[style, { width: buttonWidth }]}>
+        <Text style={[buttonTextStyle]}>{text}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+Button.propTypes = {};
+
+export default Button;
---
+++ src/components/Button/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+import { colors, fonts, fontSizes, paddings } from '../../styles';
+
+export default StyleSheet.create({
+  button: {
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: paddings.SM,
+    backgroundColor: colors.BEIGE,
+  },
+  buttonSecondary: {
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: paddings.SM,
+    borderWidth: 2,
+    borderColor: colors.BEIGE,
+  },
+  buttonTextSecondary: {
+    color: colors.BEIGE,
+    fontSize: fontSizes.BODY_TEXT,
+    fontFamily: fonts.REGULAR,
+  },
+  buttonText: {
+    color: colors.ORANGE,
+    fontSize: fontSizes.BODY_TEXT,
+    fontFamily: fonts.REGULAR,
+  },
+});
