bcde7c2a130190d1e55399066b5cb2092d771032
chengjo0 <john.cheng@futurice.com>
2018-05-21 22:09:38 +0300

    Welcome view uses the refactored welcome message

--- src/containers/views/WelcomeView.js
+++ src/containers/views/WelcomeView.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
-
-import wave from '../../../assets/img/curve/curve.png';
 import rest from '../../utils/rest';
-import { Dimensions, Image, Text, View } from 'react-native';
-import { colors, fonts, fontSizes, paddings } from '../../styles';
 import Button from '../../components/Button/Button';
 import Footer from '../../components/Footer';
-import Background from '../../components/Background/Background';
+import Background from '../../components/Background';
+import WelcomeMessage from '../../components/WelcomeMessage';
 
 const mapStateToProps = state => ({
   auth: state.auth,
@@ -57,36 +54,7 @@ export class WelcomeView extends React.Component {
   render = () => {
     return (
       <Background color="blue">
-        <Text
-          style={{
-            fontSize: fontSizes.WELCOME_MESSAGE,
-            fontFamily: fonts.TITLE,
-            color: colors.WHITE,
-            lineHeight: 60,
-          }}
-        >
-          FRIEND
-        </Text>
-        <Text
-          style={{
-            fontSize: fontSizes.WELCOME_MESSAGE,
-            fontFamily: fonts.TITLE,
-            color: colors.WHITE,
-            lineHeight: 60,
-          }}
-        >
-          SHIP !
-        </Text>
-        <Text
-          style={{
-            fontSize: fontSizes.HEADING_4,
-            fontFamily: fonts.TITLE,
-            color: colors.WHITE,
-            lineHeight: 100,
-          }}
-        >
-          YEAH! & NAAAH
-        </Text>
+        <WelcomeMessage />
         <Footer>
           <Button
             text="Join"
