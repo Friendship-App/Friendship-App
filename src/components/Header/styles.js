431e70dfe9f5fb0d787e4b15f1c80b16df849bbe
chengjo0 <john.cheng@futurice.com>
2018-05-22 14:55:07 +0300

    Design update

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,9 +2,8 @@ import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
   header: {
-    height: 35,
     width: Dimensions.get('window').width,
-    paddingHorizontal: 10,
+    padding: 15,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
