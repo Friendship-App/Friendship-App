181e69307c1422fe5bb0f51357e33e4d2de74bd9
chengjo0 <john.cheng@futurice.com>
2018-05-22 15:39:53 +0300

    Useless code removed, sign in view refactored

--- src/containers/views/SignIn/SignInView.js
+++ src/containers/views/SignIn/SignInView.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  ScrollView,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import { KeyboardAvoidingView, Text, View } from 'react-native';
 import { connect } from 'react-redux';
-import styled from 'styled-components/native';
 import { NavigationActions } from 'react-navigation';
 import HideWithKeyboard from 'react-native-hide-with-keyboard';
 
 import rest from '../../../utils/rest';
-import {
-  Centered,
-  Padding,
-  ViewContainer,
-} from '../../../components/Layout/Layout';
 import Input from '../../../components/Input/Input';
-import RoundTab from '../../../components/RoundTab';
 import Background from '../../../components/Background';
 import Footer from '../../../components/Footer';
 import { paddings } from '../../../styles';
@@ -31,18 +17,10 @@ import { paddings } from '../../../styles';
  */
 const mapStateToProps = state => ({
   auth: state.auth,
-  keyboardOpen: state.keyboardState ? state.keyboardState.keyboardOpen : false,
-  keyboardKey: state.keyboardState ? state.keyboardState.keyboardKey : 'test',
 });
 
 // Map functions to props
 const mapDispatchToProps = dispatch => ({
-  /**
-   * Call signIn action that tries logging in with the redux-api library
-   * If it's successful we wil navigate to another page
-   * Else we will log the errors
-   * @param credentials
-   */
   signIn: credentials => {
     dispatch(rest.actions.auth({}, { body: JSON.stringify(credentials) }))
       .then(() =>
@@ -55,35 +33,9 @@ const mapDispatchToProps = dispatch => ({
       )
       .catch(err => console.log(err));
   },
-  /**
-   * Navigates to the SignUpLocation screen
-   */
-  openSignUp: () =>
-    dispatch(
-      NavigationActions.navigate({
-        routeName: 'SignUp',
-      }),
-    ),
-  /**
-   * Navigates to the Welcome screen
-   */
-  openWelcomeScreen: () =>
-    dispatch(
-      NavigationActions.navigate({
-        routeName: 'Welcome',
-      }),
-    ),
 });
 
 class SignInView extends React.Component {
-  /**
-   * Disable headers
-   * @type {{title: string, header: (()=>null)}}
-   */
-  static navigationOptions = {
-    title: 'Sign up',
-  };
-
   state = {
     email: '',
     password: '',
@@ -96,11 +48,6 @@ class SignInView extends React.Component {
     this.setState({ error: true });
   }
 
-  /**
-   * After we try logging in trough or redux-api call
-   * We want to render errors from the back-end accordingly
-   * @returns {XML}
-   */
   renderStatus() {
     if (this.state.validationError) {
       return (
@@ -113,11 +60,7 @@ class SignInView extends React.Component {
       status = `Signed in as ${data.decoded.email}`;
     }
     if (this.state.error && error) {
-      if (this.state.email === '' || this.state.password === '') {
-        status = 'Please enter an e-mail and password';
-      } else {
-        status = `${error.message}`;
-      }
+      status = `${error.message}`;
     }
     if (loading) {
       status = `Loading ...`;
@@ -130,39 +73,7 @@ class SignInView extends React.Component {
     );
   }
 
-  /**
-   * Renders SignInButton
-   * Changes position when keyboard is open and closed
-   * @returns {XML}
-   */
-  renderSignInButton() {
-    if (this.props.keyboardOpen) {
-      return (
-        <View
-          onPress={() => this.signIn()}
-          style={{ position: 'absolute', bottom: 0 }}
-        >
-          <RoundTab title="Sign In" onPress={() => this.signIn()} />
-        </View>
-      );
-    }
-
-    return (
-      <View onPress={() => this.signIn()}>
-        <RoundTab title="Sign In" onPress={() => this.signIn()} />
-      </View>
-    );
-  }
-
-  /**
-   * This function will create a validation error when
-   * username or password are empty.
-   * Else it will call the redux-api function which tries to sign in the user
-   * based on the email and password that is set in the components state
-   * @returns {*|SignInView}
-   */
   signIn() {
-    Keyboard.dismiss();
     const { email, password } = this.state;
     if (!email || !password) {
       return this.setState({
@@ -172,26 +83,6 @@ class SignInView extends React.Component {
     this.props.signIn({ email, password });
   }
 
-  /**
-   * This handler will be called when pressing the TouchableWithoutFeedback
-   * This way we can handle clicks and close the keyboard accordingly
-   * We want to close the keyboard when clicking anywhere but the sign in button
-   */
-  viewClickHandler = () => {
-    if (this.props.keyboardOpen) {
-      Keyboard.dismiss();
-    }
-  };
-
-  /**
-   * Render the component.
-   * KeyboardShouldPersistTaps should be set to 'always' for the ViewContainer
-   * this way the keyboard in this view will not dismiss automatically.
-   * We handle the keyboard closing our own with a TouchableWithoutFeedback
-   * which calls the viewClickHandler function
-   *    - The keyboard will only close when pressing anywhere but the sign-in button
-   * @returns {XML}
-   */
   render() {
     return (
       <KeyboardAvoidingView
@@ -217,101 +108,28 @@ class SignInView extends React.Component {
               }}
               title="EMAIL"
               style={{ marginBottom: 30 }}
+              handleChange={value => this.setState({ email: value })}
             />
             <Input
               inputProps={{
                 placeholder: '*************',
-                secureTextEntry: true,
               }}
+              secureTextEntry
               title="PASSWORD"
+              handleChange={value => this.setState({ password: value })}
             />
+            {this.renderStatus()}
           </View>
         </Background>
-        {this.renderStatus()}
         <Footer onPress={() => this.signIn()}>
           <Text>Log in</Text>
         </Footer>
-        {/*<ViewContainer keyboardShouldPersistTaps="always">
-          <TouchableWithoutFeedback onPress={this.viewClickHandler}>
-            <Padding style={{ flex: 1 }}>
-              <Centered style={{ flex: 2, paddingBottom: 40 }}>
-                <TextInput
-                  titleColor="#f9f7f6"
-                  title="EMAIL"
-                  returnKeyType={'next'}
-                  placeholder="HELLO@FRIENDSHIP.COM"
-                  backColor="#faf6f0"
-                  keyboardType={'email-address'}
-                  onSubmitEditing={() => {
-                    this._passwordInput.focus();
-                  }}
-                  onChangeText={email =>
-                    this.setState({ email, validationError: '', error: false })}
-                  value={this.state.email}
-                  autoCapitalize={'none'}
-                />
-                <TextInput
-                  ref={component => (this._passwordInput = component)}
-                  secure={this.state.passwordSecure}
-                  password
-                  title="PASSWORD"
-                  titleColor="#f9f7f6"
-                  placeholder="*******"
-                  backColor="#faf6f0"
-                  showPassword={() =>
-                    this.setState({
-                      passwordSecure: !this.state.passwordSecure,
-                    })}
-                  onChangeText={password =>
-                    this.setState({
-                      password,
-                      validationError: '',
-                      error: false,
-                    })}
-                  value={this.state.password}
-                  returnKeyType={'done'}
-                  onSubmitEditing={() => {
-                    this.signIn();
-                  }}
-                />
-                <HideWithKeyboard>
-                  <Text style={styles.textStyle}>
-                    Need help with your password?
-                  </Text>
-                </HideWithKeyboard>
-                {this.renderStatus()}
-              </Centered>
-            </Padding>
-          </TouchableWithoutFeedback>
-          {this.renderSignInButton()}
-        </ViewContainer>*/}
       </KeyboardAvoidingView>
     );
   }
 }
 
-const HeaderWrapper = styled.View`
-  margin-top: 20;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
 const styles = {
-  headerText: {
-    fontFamily: 'NunitoSans-SemiBold',
-    fontSize: 20,
-    color: '#ff8a65',
-  },
-  buttonTextStyle: {
-    width: 230,
-    height: 27,
-    fontSize: 20,
-    fontWeight: 'bold',
-    fontFamily: 'NunitoSans-Regular',
-    textAlign: 'center',
-    color: 'white',
-  },
   statusTextStyle: {
     fontFamily: 'NunitoSans-Regular',
     width: '100%',
@@ -319,16 +137,7 @@ const styles = {
     fontSize: 15,
     textAlign: 'center',
     color: '#f673f7',
-    marginBottom: 10,
-  },
-  textStyle: {
-    fontFamily: 'NunitoSans-Regular',
-    width: '100%',
-    height: 20,
-    fontSize: 15,
-    textAlign: 'center',
-    color: '#f9f7f6',
-    marginBottom: 10,
+    marginTop: 20,
   },
 };
 
