c83042e3718c659875149d4eb721e6c89cc643c2
chengjo0 <john.cheng@futurice.com>
2018-05-22 08:37:42 +0300

    Initial commit

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TextInput extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+TextInput.propTypes = {};
+
+export default TextInput;
