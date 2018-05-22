1f4ee1869b8a52cac3e2c595668dcdc7b622f29f
chengjo0 <john.cheng@futurice.com>
2018-05-22 14:32:01 +0300

    Initial commit

---
+++ src/containers/HeaderContainer/HeaderContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class HeaderContainer extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+HeaderContainer.propTypes = {};
+
+export default HeaderContainer;
