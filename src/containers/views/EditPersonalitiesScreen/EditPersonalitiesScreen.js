5d62e10c6c3319f29d7e217524b96b938308ef59
chengjo0 <john.cheng@futurice.com>
2018-05-23 08:45:25 +0300

    Edit personalities connected to redux

--- src/containers/views/EditPersonalitiesScreen/EditPersonalitiesScreen.js
+++ src/containers/views/EditPersonalitiesScreen/EditPersonalitiesScreen.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import rest from '../../../utils/rest';
+import { connect } from 'react-redux';
+import Background from '../../../components/Background';
+
+const mapStateToProps = state => ({
+  personalities: state.personalities,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getPersonalities: () => dispatch(rest.actions.personalities()),
+});
 
 class EditPersonalitiesScreen extends Component {
+  componentWillMount() {
+    this.props.getPersonalities();
+  }
+
   render() {
-    return <div />;
+    return (
+      <Background scrollable>
+        <EditPersonalitiesList />
+      </Background>
+    );
   }
 }
 
 EditPersonalitiesScreen.propTypes = {};
 
-export default EditPersonalitiesScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(
+  EditPersonalitiesScreen,
+);
