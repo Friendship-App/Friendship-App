2ea059e0e45b26c991990ecf7cf99d16f0325f2d
chengjo0 <john.cheng@futurice.com>
2018-05-23 14:34:58 +0300

    fetch user personalities

--- src/containers/views/EditPersonalitiesScreen/EditPersonalitiesScreen.js
+++ src/containers/views/EditPersonalitiesScreen/EditPersonalitiesScreen.js
@@ -2,13 +2,67 @@ import React, { Component } from 'react';
 import Background from '../../../components/Background';
 import EditPersonalitiesList from '../../../components/EditPersonalitiesList';
 import Footer from '../../../components/Footer';
-import { Text } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native';
+import rest from '../../../utils/rest';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  userPersonalities: state.personalitiesForUser,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getUserPersonalities: userId =>
+    dispatch(rest.actions.personalitiesForUser.get({ userId })),
+});
 
 class EditPersonalitiesScreen extends Component {
+  state = {
+    selectedPersonalities: [],
+  };
+
+  componentWillMount() {
+    // const userId = this.props.navigation.state.params.personId;
+    this.props.getUserPersonalities(1).then(data => this.prepareData(data));
+  }
+
+  prepareData = selectedPersonalities => {
+    let data = [];
+    selectedPersonalities.map(personality =>
+      data.push(personality.personalityId),
+    );
+    this.setState({ selectedPersonalities: data });
+  };
+
+  updatePersonalities = (oldPersonality, newPersonality) => {
+    let newSelectedPersonalities = this.state.selectedPersonalities;
+    let pos = newSelectedPersonalities.indexOf(oldPersonality);
+    if (pos > -1) {
+      newSelectedPersonalities.splice(pos, 1, newPersonality);
+    } else {
+      newSelectedPersonalities.push(newPersonality);
+    }
+    newSelectedPersonalities.sort();
+    this.setState({ selectedPersonalities: newSelectedPersonalities });
+  };
+
   render() {
+    const { userPersonalities } = this.props;
+    const { selectedPersonalities } = this.state;
+
+    if (
+      userPersonalities.loading ||
+      !userPersonalities.sync ||
+      !selectedPersonalities
+    ) {
+      return <ActivityIndicator />;
+    }
+
     return (
       <Background>
-        <EditPersonalitiesList />
+        <EditPersonalitiesList
+          updatePersonalities={this.updatePersonalities}
+          selectedPersonalities={selectedPersonalities}
+        />
         <Footer secondary>
           <Text>SAVE</Text>
         </Footer>
@@ -19,4 +73,6 @@ class EditPersonalitiesScreen extends Component {
 
 EditPersonalitiesScreen.propTypes = {};
 
-export default EditPersonalitiesScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(
+  EditPersonalitiesScreen,
+);
