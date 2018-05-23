72e1d77b0613de59506d1d448d93fab4da598759
chengjo0 <john.cheng@futurice.com>
2018-05-23 09:21:07 +0300

    Footer added

--- src/containers/views/EditPersonalitiesScreen/EditPersonalitiesScreen.js
+++ src/containers/views/EditPersonalitiesScreen/EditPersonalitiesScreen.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { View } from 'react-native';
-import rest from '../../../utils/rest';
-import { connect } from 'react-redux';
 import Background from '../../../components/Background';
 import EditPersonalitiesList from '../../../components/EditPersonalitiesList';
-
-const mapStateToProps = state => ({
-  personalities: state.personalities,
-});
-
-const mapDispatchToProps = dispatch => ({
-  getPersonalities: () => dispatch(rest.actions.personalities()),
-});
+import Footer from '../../../components/Footer';
+import { Text } from 'react-native';
 
 class EditPersonalitiesScreen extends Component {
-  componentWillMount() {
-    this.props.getPersonalities();
-  }
-
   render() {
     return (
       <Background scrollable>
         <EditPersonalitiesList />
+        <Footer>
+          <Text>SAVE</Text>
+        </Footer>
       </Background>
     );
   }
@@ -30,6 +19,4 @@ class EditPersonalitiesScreen extends Component {
 
 EditPersonalitiesScreen.propTypes = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(
-  EditPersonalitiesScreen,
-);
+export default EditPersonalitiesScreen;
