f6a5855ec6ee7886e59529debf7a3da526b0a37d
chengjo0 <john.cheng@futurice.com>
2018-05-23 08:33:30 +0300

    Initial commit

---
+++ src/containers/views/EditPersonalitiesScreen/EditPersonalitiesScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class EditPersonalitiesScreen extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+EditPersonalitiesScreen.propTypes = {};
+
+export default EditPersonalitiesScreen;
