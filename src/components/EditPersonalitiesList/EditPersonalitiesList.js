8213bf7ba08d053f12394d2fdd375a2a294cefb3
chengjo0 <john.cheng@futurice.com>
2018-05-23 09:21:24 +0300

    Flatlist shows all the personalities

--- src/components/EditPersonalitiesList/EditPersonalitiesList.js
+++ src/components/EditPersonalitiesList/EditPersonalitiesList.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ActivityIndicator, FlatList } from 'react-native';
+import rest from '../../utils/rest';
+import { connect } from 'react-redux';
+import Personality from '../SignUp/Personality';
+
+const mapStateToProps = state => ({
+  personalities: state.personalities,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getPersonalities: () => dispatch(rest.actions.personalities()),
+});
+
+const renderItem = ({ item }) => {
+  console.log(item);
+  return (
+    <Personality
+      title={item.name}
+      image={item.name}
+      onPress={() => {
+        console.log('clicked');
+      }}
+    />
+  );
+};
+
+const keyExtractor = personality => `personality-${personality.id}`;
 
 class EditPersonalitiesList extends Component {
+  componentWillMount() {
+    this.props.getPersonalities();
+  }
+
   render() {
-    return null;
+    const { personalities } = this.props;
+
+    if (personalities.loading || !personalities.sync) {
+      return <ActivityIndicator />;
+    }
+
+    return (
+      <FlatList
+        data={this.props.personalities.data}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={{ width: '100%' }}
+      />
+    );
   }
 }
 
 EditPersonalitiesList.propTypes = {};
 
-export default EditPersonalitiesList;
+export default connect(mapStateToProps, mapDispatchToProps)(
+  EditPersonalitiesList,
+);
