68f18330c87f10bb69b33cb90b1c8dd9b82a90d1
chengjo0 <john.cheng@futurice.com>
2018-05-23 08:47:38 +0300

    Initial commit

---
+++ src/components/EditPersonalitiesList/EditPersonalitiesList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class EditPersonalitiesList extends Component {
+  render() {
+    return null;
+  }
+}
+
+EditPersonalitiesList.propTypes = {};
+
+export default EditPersonalitiesList;
