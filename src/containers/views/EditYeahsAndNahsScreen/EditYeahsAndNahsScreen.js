df09a2e797d0f5347bf9cbf32822477ef2e7ce5d
chengjo0 <john.cheng@futurice.com>
2018-05-24 10:29:01 +0300

    Get user tags done

--- src/containers/views/EditYeahsAndNahsScreen/EditYeahsAndNahsScreen.js
+++ src/containers/views/EditYeahsAndNahsScreen/EditYeahsAndNahsScreen.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
+import rest from '../../../utils/rest';
+import { connect } from 'react-redux';
+
+const mapDispatchToProps = dispatch => ({
+  getUserTags: userId => dispatch(rest.actions.tagsForUser.get({ userId })),
+});
+
+const mapStateToProps = state => ({
+  userTags: state.tagsForUser,
+});
 
 class EditYeahsAndNahsScreen extends Component {
+  state = {
+    yeahs: [],
+    nahs: [],
+  };
+
+  componentWillMount() {
+    const userId = this.props.userId;
+    this.props.getUserTags(userId).then(data => this.prepareData(data));
+  }
+
+  prepareData = userTags => {
+    let yeahs = [];
+    let nahs = [];
+    userTags.map(tag => {
+      console.log(`name : ${tag.name}         love : ${tag.love}`);
+      tag.love ? yeahs.push(tag) : nahs.push(tag);
+    });
+    this.setState({ yeahs, nahs });
+  };
+
   render() {
-    return <View />;
+    console.log(this.state);
+    const { userTags } = this.props;
+    const { yeahs, nahs } = this.props;
+
+    if (userTags.loading || !userTags.sync || !yeahs || !nahs) {
+      return <ActivityIndicator />;
+    }
+    return <EditTagsList />;
   }
 }
 
 EditYeahsAndNahsScreen.propTypes = {};
 
-export default EditYeahsAndNahsScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(
+  EditYeahsAndNahsScreen,
+);
