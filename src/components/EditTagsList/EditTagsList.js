75985911bcfb6f015d1dd68aa9421d7016b5ebb0
chengjo0 <john.cheng@futurice.com>
2018-05-24 10:28:14 +0300

    Initial commit

---
+++ src/components/EditTagsList/EditTagsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class EditTagsList extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+EditTagsList.propTypes = {};
+
+export default EditTagsList;
