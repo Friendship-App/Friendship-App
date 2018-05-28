956a9a67de5d51f16b74dc691713452f246e344a
chengjo0 <john.cheng@futurice.com>
2018-05-28 11:11:52 +0300

    uses Background, HeaderEvent design fix

--- src/components/Events/EventsHeader.js
+++ src/components/Events/EventsHeader.js
@@ -19,7 +19,6 @@ const styles = {
     justifyContent: 'flex-start',
     alignItems: 'center',
     height: 60,
-    paddingTop: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
