8d1ee29385a5fbc634f2cf5ec150de3d88d19c8a
chengjo0 <john.cheng@futurice.com>
2018-05-28 12:09:00 +0300

    Background updated

--- src/containers/views/Chat/ChatInbox.js
+++ src/containers/views/Chat/ChatInbox.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 
 import rest from '../../../utils/rest';
 import RoundTab from '../../../components/RoundTab';
 import InboxCard from '../../../components/InboxCard';
-import SuggestionList from '../../../components/SuggestionList';
 import Report from '../Report/Report';
 import { NavigationActions } from 'react-navigation';
+import Background from '../../../components/Background';
+import { paddings } from '../../../styles';
 
 const mapStateToProps = state => ({
   currentUserId: state.auth.data.decoded ? state.auth.data.decoded.id : null,
@@ -56,34 +57,36 @@ export class ChatInbox extends React.Component {
         })
       : [];
     return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
-        <RoundTab tint="#ffffff" title="CHATS" fontSize="12" />
-        {sortedChatrooms.length > 0 ? (
-          <FlatList
-            data={sortedChatrooms}
-            keyExtractor={this.keyExtractor}
-            renderItem={this.renderItem}
-            style={{ flex: 1, backgroundColor: 'white', minHeight: 300 }}
-          />
-        ) : (
-          <View style={{ flex: 1, backgroundColor: '#fff', padding: 20 }}>
-            <Text style={{ fontWeight: 'bold', paddingBottom: 20 }}>
-              Get busy chatting!
-            </Text>
-            <Text style={{ paddingBottom: 20 }}>
-              You have no active chats yet. To start a new chat, first find a
-              person that seems interesting to you. Open their profile, see what
-              you have in common and take it from there.
-            </Text>
-            <Text
-              style={{ textDecorationLine: 'underline', color: '#ff8a65' }}
-              onPress={() => this.props.goToPeopleView()}
-            >
-              Browse profiles
-            </Text>
-          </View>
-        )}
-      </View>
+      <Background color="grey">
+        <View style={{ flex: 1, marginTop: paddings.SM }}>
+          <RoundTab tint="#ffffff" title="CHATS" fontSize="12" />
+          {sortedChatrooms.length > 0 ? (
+            <FlatList
+              data={sortedChatrooms}
+              keyExtractor={this.keyExtractor}
+              renderItem={this.renderItem}
+              style={{ flex: 1, backgroundColor: 'white', minHeight: 300 }}
+            />
+          ) : (
+            <View style={{ flex: 1, backgroundColor: '#fff', padding: 20 }}>
+              <Text style={{ fontWeight: 'bold', paddingBottom: 20 }}>
+                Get busy chatting!
+              </Text>
+              <Text style={{ paddingBottom: 20 }}>
+                You have no active chats yet. To start a new chat, first find a
+                person that seems interesting to you. Open their profile, see
+                what you have in common and take it from there.
+              </Text>
+              <Text
+                style={{ textDecorationLine: 'underline', color: '#ff8a65' }}
+                onPress={() => this.props.goToPeopleView()}
+              >
+                Browse profiles
+              </Text>
+            </View>
+          )}
+        </View>
+      </Background>
     );
   }
 }
