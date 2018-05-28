648998a81bdfcde73d8ed337948f926021a69afd
chengjo0 <john.cheng@futurice.com>
2018-05-28 11:47:58 +0300

    styling refactored

--- src/components/EventsHeader/EventsHeader.js
+++ src/components/EventsHeader/EventsHeader.js
@@ -1,39 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import styles from './styles';
 
 const EventsHeader = ({ headerText, rightText }) => {
-  const { textStyle, viewStyle, rightStyle } = styles;
-
   return (
-    <View style={viewStyle}>
-      <Text style={textStyle}>{headerText}</Text>
-      <View style={rightStyle}>{rightText}</View>
+    <View style={[styles.viewStyle]}>
+      <Text style={[styles.textStyle]}>{headerText}</Text>
+      <View style={[styles.rightStyle]}>{rightText}</View>
     </View>
   );
-};
-
-const styles = {
-  viewStyle: {
-    backgroundColor: '#F8F8F8',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    height: 60,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative',
-  },
-  textStyle: {
-    fontSize: 20,
-    flex: 60,
-    justifyContent: 'center',
-    marginLeft: 20,
-  },
-  rightStyle: {
-    flex: 40,
-  },
 };
 
 export default EventsHeader;
